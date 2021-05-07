9e9fdfe3a96ecdc113154a66af624596d36bf3c5
rizwanullah313 <ru323091@gmail.com>
2021-05-07 10:25:06 -0700

    Start with python

---
+++ task.py
@@ -0,0 +1,21 @@
+
+# youtuber = "rizwan"
+
+# print("subscriber to "+youtuber)
+# print("Subscriber to {}".format(youtuber))
+# print(f"subscriber to {youtuber}")
+
+
+# adj = input("Adjective: ")
+# verb1 = input("Verb: ")
+# verb2 = input("Verb: ")
+# famous_person = input("Famous person: ")
+
+# maltlib = f"computer progamming is so {adj}! It makes me so excited all the time beacuse \ " \
+#          f"I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+# print(maltlib)
+
+
+
+
