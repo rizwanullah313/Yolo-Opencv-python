cfdf484903fe926dfaa6304c79608d100c595724
rizwanullah313 <ru323091@gmail.com>
2021-05-22 16:41:13 +0500

    successfully complete image recognition project

--- Carimagedetection&Classifier/imageclassifierfeaturedetectors.py
+++ Carimagedetection&Classifier/imageclassifierfeaturedetectors.py
@@ -23,8 +23,44 @@ def findDes(images):
         desList.append(des)
     return desList
 
+def finID(img, desList, thres=15):
+    kp2, des2 = orb.detectAndCompute(img, None)
+    bf = cv2.BFMatcher()
+    matchList=[]
+    finalVal = -1
+    try:
+        for des in desList:
+           matches = bf.knnMatch(des, des2, k=2)
+           good = []
+           for m, n in matches:
+            if m.distance < 0.75*n.distance:
+                good.append([m])
+        matchList.append(len(good))
+       # print(matchList)
+    except:
+        pass
+    if len(matchList)!=0:
+        if max(matchList) > thres:
+            finalVal = matchList.index(max(matchList))
+    return finalVal
+
 desList = findDes(images)
 print(len(desList))
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img2 = cap.read()
+    imgOriginal = img2.copy()
+    img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    id = finID(img2, desList)
+    if id != -1:
+        cv2.putText(imgOriginal, classNames[id],(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),1)
+
+    cv2.imshow('img2', imgOriginal)
+    cv2.waitKey(0)
+
+
 # img1 = cv2.imread("Qiamges/honda civic 4.jpg", 0)
 # img2 = cv2.imread("Tiamge/honda civic 6.jpg", 0)
 
@@ -36,13 +72,7 @@ print(len(desList))
 # imgKp1 = cv2.drawKeypoints(img1, kp1, None)
 # imgKp2 = cv2.drawKeypoints(img2, kp2, None)
 
-# bf = cv2.BFMatcher()
-# matches = bf.knnMatch(des1, des2, k=2)
-# good = []
-# for m, n in matches:
-   # if m.distance < 0.75*n.distance:
-  #      good.append([m])
-# print(len(good))
+
 # img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
 # cv2.imshow("ImgKp1", imgKp1)
 # cv2.imshow("ImgKp2", imgKp2)
